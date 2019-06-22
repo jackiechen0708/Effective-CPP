c02c82f004b7441496412ab5f3096d873f7ced1d
jackiechen0708 <12307130250@fudan.edu.cn>
2019-06-22 14:31:11 +0800

    Update item 1

---
+++ View C++ as a federation of languages.cpp
@@ -0,0 +1,40 @@
+// C Part
+/*
+1.blocks
+2.statements
+3.preprocessor
+4.built-in data types
+5.arrays
+6.pointers
+*/
+
+// C++ Part
+/*
+1.exceptions
+2.overloading
+*/
+
+// Object-Oriented C++ Part
+/*
+1.classes
+2.encapsulation
+3.inheritance
+4.polymoriphism
+5.virtual
+*/
+
+// Template C++ Part
+/*
+1.generic programming
+2.template meta programming
+*/
+
+// STL Part
+/*
+1.containers
+2.iterators
+3.algorithms
+4.function objects
+*/
+
+// C++高效编程方法决定于你使用C++哪一部分
